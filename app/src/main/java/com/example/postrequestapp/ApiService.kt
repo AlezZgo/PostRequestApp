e48926a820d09f7af8aefd1274cc3b228ef10c14
Alexander <alez.grante@gmail.com>
2022-02-25 00:35:55 +0300

    post request formed with no file exception

--- app/src/main/java/com/example/postrequestapp/ApiService.kt
+++ app/src/main/java/com/example/postrequestapp/ApiService.kt
@@ -1,4 +1,15 @@
 package com.example.postrequestapp
 
+import okhttp3.MultipartBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
 interface ApiService {
+
+    @Multipart
+    @POST("qrgen")
+    fun generate(@Part filePart: MultipartBody.Part): Call<ResponseBody>
 }
--- app/src/main/java/com/example/postrequestapp/MainActivity.kt
+++ app/src/main/java/com/example/postrequestapp/MainActivity.kt
@@ -1,14 +1,94 @@
 package com.example.postrequestapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.example.postrequestapp.databinding.ActivityMainBinding
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.OkHttpClient
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.ResponseBody
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var uri: Uri
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val api = initApi()
+
+        val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) {
+            uri = it
+            binding.pathField.text = uri.toString()
+        }
+
+        binding.pickImageButton.setOnClickListener {
+            getContent.launch("image/*")
+        }
+
+        binding.requestButton.setOnClickListener {
+            val file = File(uri.path!!)
+            val formData = MultipartBody.Part.createFormData(
+                "file",
+                file.absolutePath,
+                file.asRequestBody("multipart/form-data".toMediaType())
+            )
+
+            api.generate(formData).enqueue(object : Callback<ResponseBody> {
+                override fun onResponse(
+                    call: Call<ResponseBody>,
+                    response: Response<ResponseBody>,
+                ) {
+                    if (response.isSuccessful)
+                        Log.i("loger", response.body().toString())
+                }
+
+                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                    Log.i("loger", t.message.toString())
+                }
+
+
+            })
+        }
 
 
     }
+
+    private fun initApi(): ApiService {
+        val interceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(baseUrl)
+            .client(client)
+            .build()
+
+        return retrofit.create(ApiService::class.java)
+    }
+
+    companion object {
+        private const val baseUrl = "https://qrcone.herokuapp.com/"
+    }
+
+
 }
